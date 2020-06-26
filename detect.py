33790e99089d2188d76bb2cd9cf3b434c4b307a6
Saswat <saswatsubhajyotimallick@gmail.com>
2020-06-26 13:20:24 +0530

    minor bug fix

--- detect.py
+++ detect.py
@@ -132,7 +132,7 @@ def detect(save_img=False):
                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
